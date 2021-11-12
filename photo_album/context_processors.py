e8b469dc497a6ec4c821ce4ed8e566631e94bf6a
shondal <the.count@s-aws.com>
2021-11-12 08:39:11 -0500

    Update context_processors.py
    Saturation to 150

--- photo_album/context_processors.py
+++ photo_album/context_processors.py
@@ -16,7 +16,7 @@ def consts(request):
             "class": "thumbnail inline", "overlay": "ktwsgtfb7pflzhhowhxp", "format": "jpg", "crop": "fill",
         },
         THUMBNAIL_SUPERSATURATION = {
-            "class": "thumbnail inline", "format": "jpg", "crop": "fill", "height": 150, "width": 150, "effect": "saturation:100",
+            "class": "thumbnail inline", "format": "jpg", "crop": "fill", "height": 150, "width": 150, "effect": "saturation:150",
         },
         CLOUDINARY_CLOUD_NAME = cloudinary.config().cloud_name
     )
