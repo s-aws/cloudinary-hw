59019afa0e0b82896402e13f5028d9375f39ea1e
S <support@shondal.net>
2021-11-23 04:09:52 -0500

    recommended changes

--- cloudinary_django_sample/urls.py
+++ cloudinary_django_sample/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     url(r'^upload/complete$', photo_album.direct_upload_complete, name='photo_album.views.direct_upload_complete'),
     # Add the admin functionality:
     url(r'^admin/', admin.site.urls),
+    url(r'^delete/.*$', photo_album.delete, name='photo_album.views.delete'),
 ]
